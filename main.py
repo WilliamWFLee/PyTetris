eb858350376b93ed05c448f41c3cbef878aa773c
William Lee <wlee753159@gmail.com>
2020-08-20 21:50:10 +0100

    Add comments

--- main.py
+++ main.py
@@ -34,34 +34,43 @@ from typing import List, Optional
 
 import pygame
 
+# Various game dimensions
 VISIBLE_ROWS = 20
 ROWS = 40
 COLUMNS = 10
 SQUARE_SIZE = 30
 LINE_WIDTH = 1
 
+# The length of time before a shape locks
 LOCK_DELAY = 500
 
 # Padding around the grid, as the number of squares,
 # left and right, and up and down respectively
 PADDING = (3, 4)
 
+# Named tuples to make things easier to read
 Dimensions = namedtuple("Dimensions", "width height")
 Position = namedtuple("Position", "x y")
 
+# The size of the display surface
 DISPLAY_SIZE = Dimensions(
     *((v + 2 * pad) * SQUARE_SIZE for v, pad in zip((COLUMNS, VISIBLE_ROWS), PADDING))
 )
+# The size of the playfield
 PLAYFIELD_SIZE = Dimensions(COLUMNS * SQUARE_SIZE, ROWS * SQUARE_SIZE)
+# The visible size of the playfield
 VISIBLE_PLAYFIELD_SIZE = Dimensions(COLUMNS * SQUARE_SIZE, VISIBLE_ROWS * SQUARE_SIZE)
+# The position of the playfield relative to the display surface
 GRID_POS = Position(*(pad * SQUARE_SIZE for pad in PADDING))
+# The position that new tetrominoes appear
 SPAWN_POS = Position(3, 21)
 
+# Color tuples
 WHITE = 3 * (255,)
 BLACK = 3 * (0,)
 GREY = 3 * (127,)
 
-
+# Colors of the tetrominoes
 COLORS = tuple(
     pygame.Color(color)
     for color in ("cyan", "yellow", "purple", "green", "red", "blue", "orange")
@@ -69,6 +78,9 @@ COLORS = tuple(
 
 
 class BlockType(Enum):
+    """
+    Enumeration of the block types
+    """
     IBlock = 0
     JBlock = 1
     LBlock = 2
@@ -77,7 +89,7 @@ class BlockType(Enum):
     TBlock = 5
     ZBlock = 6
 
-
+# Wall kick definitions
 JLSTZ_WALL_KICKS = {
     0: {
         1: [(-1, 0), (-1, 1), (0, -2), (-1, -2)],
@@ -123,7 +135,7 @@ WALL_KICKS = {
     BlockType.ZBlock: JLSTZ_WALL_KICKS,
 }
 
-
+# Tetromino definitions
 # fmt: off
 BLOCKS = {
     BlockType.IBlock: ["    ",
@@ -193,6 +205,7 @@ class Tetromino:
         return True
 
     def _remove_from_grid(self):
+        # Removes this shape from the grid
         for y, row in enumerate(self.block):
             for x, square in enumerate(row):
                 if (
@@ -203,6 +216,7 @@ class Tetromino:
                     self.grid[self.y + y][self.x + x] = 0
 
     def _can_move(self, dx: int, dy: int) -> bool:
+        # Determines whether this shape can move in a given direction
         for y, row in enumerate(self.block):
             for x, square in enumerate(row):
                 if not square == ".":
@@ -231,6 +245,9 @@ class Tetromino:
         return can_move if test_move else None
 
     def _rotate(self, amount: int) -> bool:
+        # Rotates a shape by a certain amount
+        # Rotations as multiples of 90 degrees clockwise, so a rotation of 1
+        # is 90 degrees clockwise, -1 is 90 degrees anticlockwise
         amount %= 4
         old_rotation = self.rotation
         self.rotation = (self.rotation + amount) % 4
@@ -240,7 +257,7 @@ class Tetromino:
                 for x in range(len(self.block[0]))
             ]
 
-        if not self._can_place():
+        if not self._can_place():  # Normal rotation cannot be performed
             try:
                 wall_kicks = WALL_KICKS[self.block_type][old_rotation][self.rotation]
             except KeyError:
@@ -251,7 +268,7 @@ class Tetromino:
                         self._move(dx=dx, dy=dy, test_move=False)
                         return True
 
-            self._rotate(-amount)
+            self._rotate(-amount)  # Undoes the rotation
             return False
         return True
 
@@ -319,12 +336,13 @@ class Tetris:
             pass
 
     @staticmethod
-    def _generate_tetrominoes():
+    def _generate_tetrominoes():  # Implements the Random Generator
         tetrominoes = list(BlockType)
         random.shuffle(tetrominoes)
         return tetrominoes
 
     def draw_grid(self):
+        # Draws the grid and the squares on the board
         grid_surface = pygame.Surface(VISIBLE_PLAYFIELD_SIZE)
         for y, row in enumerate(self.grid[-VISIBLE_ROWS:]):
             for x, square in enumerate(row):
