d56af89e9bd918b5cb19b93556f422f07b4faece
William Lee <wlee753159@gmail.com>
2020-08-20 11:12:21 +0100

    Fix: IBlock repr

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ BLOCKS = {
     BlockType.IBlock: ["    ",
                        "....",
                        "    ",
-                       "...."],
+                       "    "],
     BlockType.JBlock: [".  ",
                        "...",
                        "   "],
